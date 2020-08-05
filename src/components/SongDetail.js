878b5eeb01a65763d44d2fca1ed5eb83eb9e5a64
Andrew Andersen <andersac88@gmail.com>
2020-08-04 22:04:38 -0500

    can't help myself

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = ({selectedSong}) => {
-    if (!selectedSong) {
-        return <div>Select a Song</div>
-    }
+const SongDetail = ({ selectedSong }) => {
+	if (!selectedSong) {
+		return <div>Select a Song</div>;
+	}
 	return (
-    <div>
-        <h3>Details for:</h3>
-        <p>
-        Title:{selectedSong.title}
-        <br />
-        Duration: {selectedSong.duration}
-        </p>
-        </div>
-        );
+		<div>
+			<h3>Details for:</h3>
+			<p>
+				Title:{selectedSong.title}
+				<br />
+				Duration: {selectedSong.duration}
+			</p>
+		</div>
+	);
 };
 
 const mapStateToProps = (state) => {
